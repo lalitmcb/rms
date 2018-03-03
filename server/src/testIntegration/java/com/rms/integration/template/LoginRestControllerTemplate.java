edc2886867f1087d50b87759e4afc20a6284732d
lalitBhatt <lalit.bhatt>
2018-03-03 17:33:17 +0530

    First cut for integration test (still broken)

---
+++ server/src/testIntegration/java/com/rms/integration/template/LoginRestControllerTemplate.java
@@ -0,0 +1,29 @@
+package com.rms.integration.template;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rms.security.LoginVO;
+
+@Service("loginRestControllerTemplate")
+public class LoginRestControllerTemplate {
+	
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	public ResponseEntity<String> login(LoginVO loginVO) throws JsonProcessingException {
+		String loginJSON = objectMapper.writeValueAsString(loginVO);
+		HttpEntity<String> httpEntity = new HttpEntity<>(loginJSON, null);
+		return testRestTemplate.postForEntity("/login",httpEntity, String.class);
+	}
+	
+}
+
